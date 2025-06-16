4f7ba3587421f0f764440b9175a03fbad8870904
ERDEM-COULD <merdem58171.3@gmail.com>
2025-06-16 15:12:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// CORS izin verelim frontend için (istersen pakete cors da kurabilirsin)
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
+// public klasörünü statik olarak sun
+app.use(express.static(path.join(__dirname, 'public')));
+
+// IP endpointi
+app.get('/get-ip', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+  res.json({ ip: ip, message: "IP başarıyla alındı" });
 });
 
-// IP'yi JSON olarak dönen endpoint
-app.get('/get-ip', (req, res) => {
-  // Gerçek IP için x-forwarded-for kontrolü (proxy arkasındaysan)
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
-
-  // JSON olarak dönebiliriz
-  res.json({
-    ip: ip,
-    message: "IP başarıyla alındı"
-  });
+// Herhangi başka isteklerde index.html'i gönder (SPA için)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
